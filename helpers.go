cafd1a9a54ca2109a3cdcec48cf5909697e7ed70
jimfleming <jimmy.fleming@gmail.com>
2013-06-18 22:13:15 -0700

    Adding a color parser; still working on the response interface

---
+++ helpers.go
@@ -0,0 +1,38 @@
+package sphero
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+// Used to generate mask1 or mask2 for SetDataStreaming command
+func ApplyMasks32(masks []uint32) uint32 {
+	var mask uint32 = 0
+	for _, m := range masks {
+		mask |= m
+	}
+	return mask
+}
+
+func ParseColor(data []byte) (*Color, error) {
+	c := new(Color)
+	if len(data) != 3 {
+		return c, fmt.Errorf("Could not format %#x as Color", c)
+	}
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.BigEndian, &c.R)
+	binary.Read(buf, binary.BigEndian, &c.G)
+	binary.Read(buf, binary.BigEndian, &c.B)
+	return c, nil
+}
+
+// Computes the modulo 256 sum of the bytes, bit inverted (1's complement)
+func ComputeChk(data []byte) uint8 {
+	sum := 0
+	for _, b := range data {
+		sum += int(uint8(b))
+	}
+	chk := (sum % 256) ^ 0xff
+	return uint8(chk)
+}
